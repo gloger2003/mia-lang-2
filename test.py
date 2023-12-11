f0dbbd62c1ca3e01f2d943758ab4f316292cc398
gloger2003 <excelsior7554@gmail.com>
2023-12-11 16:17:46 +0300

    expressions

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from pprint import pformat, pprint
 import tokenize as tokenizer
 from tokenize import TokenInfo
 import token as T
-from typing import List, Union
+from typing import List, Optional, Tuple, Union
 
 code = b"""
 var int a;
@@ -41,14 +41,33 @@ class KEYWORDS(enum.Enum):
         return mapping.get(self)
 
 
-class TYPES(enum.Enum):
+class TypesEnum(enum.Enum):
     int = 0
     
     def get_type_class(self) -> Union['IntType', None]:
         mapping = {
-            TYPES.int: IntType
+            TypesEnum.int: IntType
         }
         return mapping.get(self)
+    
+
+class SingleOperatorsEnum(enum.Enum):
+    # =
+    assign = 0
+    
+    @classmethod
+    def get_single_operator(cls, op_str: str) -> Optional['SingleOperatorsEnum']:
+        mapping = {
+            '=': cls.assign
+        }
+        return mapping.get(op_str)
+    
+    def get_operator_class(self) -> Union['AssignSingleOperator', None]:
+        mapping = {
+            SingleOperatorsEnum.assign: AssignSingleOperator
+        }
+        return mapping.get(self)
+
 
 class IntType:
     DEFAULT_VALUE = 0
@@ -57,7 +76,28 @@ class IntType:
         self._value = __class__.DEFAULT_VALUE
 
     def __repr__(self) -> str:
-        return f'TYPE::IntType({self._value})'
+        return f'<T:IntType({self._value})>'
+    
+    
+class AssignSingleOperator:
+    COUNT_OF_ARGS = 2
+    VARIANT_ID_OP_VALUE = ('ID', '=','VALUE')
+    VARIANT_ID_OP_ID = ('ID', '=', 'ID')
+    
+    def __init__(self, variant: Tuple[str], t1: TokenInfo, t_op: TokenInfo, t2: TokenInfo):
+        self._t1 = t1
+        self._t2 = t2
+        self._t_op = t_op
+        self._variant: Tuple[str] = variant
+    
+    def to_dict(self) -> dict:
+        return {
+            'AssignSignleOperator': {   
+                'LINE': f'{self._t1.string} {self._t_op.string} {self._t2.string}',
+                'VARIANT': f'<{self._variant[0]}> {self._t_op.string} <{self._variant[2]}>'
+            }
+        }
+    
     
 class VarInstruction:
     COUNT_OF_ARGS = 2
@@ -76,7 +116,7 @@ class VarInstruction:
     def parse_tokens(self):
         v_type = self._t_args[0].string
         v_id = self._t_args[1].string
-        self.var_type_class = TYPES[v_type].get_type_class()
+        self.var_type_class = TypesEnum[v_type].get_type_class()
         self.var_type_obj = self.var_type_class()
         self.var_id = v_id
         pass
@@ -95,8 +135,7 @@ class LineNode:
     def __init__(self, _tokens: List[TokenInfo]):
         self._tokens = _tokens
         self._instruction_node: InstructionNode = None
-        self._expr_node: ExprNode = None
-        self._id_node: IdNode = None
+        self._expression_node: ExpressionNode = None
         self.parse_tokens()
 
     def __repr__(self) -> str:
@@ -107,7 +146,7 @@ class LineNode:
             'LineNode': {
                 'TOKENS': [k.string for k in self._tokens],
                 'INST_NODE': self._instruction_node.to_dict() if self._instruction_node is not None else None,
-                'EXPR_NODE': self._expr_node.to_dict() if self._expr_node is not None else None
+                'EXPR_NODE': self._expression_node.to_dict() if self._expression_node is not None else None
             }
         }
 
@@ -124,21 +163,47 @@ class LineNode:
                     self._instruction_node = InstructionNode(t, t_args)
                 else:
                     t_args = self._tokens[0:]
-                    self._expr_node = ExprNode(t, t_args)
+                    self._expression_node = ExpressionNode(t_args)
                 
 
-class ExprNode:
-    def __init__(self, t_id: TokenInfo, t_args: List[TokenInfo]) -> None:
-        self._t_id = t_id
+class ExpressionNode:
+    def __init__(self, t_args: List[TokenInfo]) -> None:
         self._t_args = t_args
+        
+        self._operator_class: Union[AssignSingleOperator, None] = None
+        self._operator_obj: Union[AssignSingleOperator, None] = None
+        # self._t1 = None
+        # self._t2 = 
+        
+        self.parse_tokens()
         
     def __repr__(self) -> str:
         return repr(self.to_dict())
+    
+    def parse_tokens(self):
+        single_operator = None
+        double_operator = None
+        
+        for i, t in enumerate(self._t_args):
+            single_operator = SingleOperatorsEnum.get_single_operator(t.string)
+            if single_operator is not None:
+                self._operator_class: AssignSingleOperator = single_operator.get_operator_class()
+                self._operator_obj = self._operator_class(
+                    self._operator_class.VARIANT_ID_OP_VALUE,
+                    self._t_args[i - 1], t, self._t_args[i + 1]
+                )
+            elif double_operator is not None:
+                break
+        
+        if single_operator is not None:
+            SingleOperatorsEnum.get_operator_class()
+        pass
         
     def to_dict(self) -> dict:
         return {
-            'ExprNode': {
-                'ARGS': [k.string for k in self._t_args]
+            'ExpressionNode': {
+                'ARGS': [k.string for k in self._t_args],
+                'OPERATOR': self._operator_obj.to_dict() if self._operator_obj is not None else None
             }
         }
 
@@ -171,17 +236,6 @@ class InstructionNode:
         }
         
     
-class ValueNode:
-    def __init__(self) -> None:
-        pass
-
-
-class IdNode:
-    def __init__(self) -> None:
-        pass
-    
-
-    
 
 f_readline = io.BytesIO(code).readline
 
@@ -200,6 +254,7 @@ for t in tokens:
         line_tokens = []
         continue
     line_tokens.append(t)
+        
         
 with open('./dump.json', 'w') as f:
     data = [k.to_dict() for k in line_nodes]
